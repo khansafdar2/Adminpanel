91eb59c0c7a72dd9d4232c544a1296d8255a9613
Atta ur Rehman <atta-ur-rehman@alchemative.com>
2021-07-19 18:31:18 +0500

    Discount create and index

--- src/app/views/discounts/discounts.component.ts
+++ src/app/views/discounts/discounts.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { Column } from 'src/app/shared/datatable/datatable.component';
 import URLS from 'src/app/shared/urls';
+import { DiscountsService } from './discounts.service';
 
 @Component({
   selector: 'app-discounts',
@@ -8,12 +11,57 @@ import URLS from 'src/app/shared/urls';
 })
 export class DiscountsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private discountsService: DiscountsService
+  ) { }
 
   loading: boolean = false;
   URLS = URLS;
+  columns: Column[] = [
+    {
+      title: "Title",
+      selector: "title"
+    },
+    {
+      title: "Type",
+      selector: "discount_type",
+      cell: row => row.discount_type === "percentage" ? "Percentage" : "Amount"
+    },
+    {
+      title: "Discount value",
+      selector: "type_value",
+      cell: row => row.discount_type === "percentage" ? row.type_value + "%" : row.type_value + "QAR"
+    }
+  ];
+  discounts = [];
+  totalCount: number = 0;
+  pageNumber: number = 1;
+  pageSize: number = 20;
+
+  getDiscountsList() {
+    this.loading = true;
+    this.discountsService.getDiscountsList().then(resp => {
+      this.loading = false;
+      if(resp) {
+        console.log(resp.data);
+        this.discounts = resp.data.results;
+        this.totalCount = resp.data.count;
+      }
+    });
+  }
+
+  onPageChange(event: PageEvent) {
+    this.pageNumber = event.pageIndex + 1;
+    this.pageSize = event.pageSize;
+    this.getDiscountsList();
+  }
+
+  onCellClick(data) {
+    
+  }
 
   ngOnInit(): void {
+    this.getDiscountsList();
   }
 
 }
--- src/app/views/discounts/discounts.service.ts
+++ src/app/views/discounts/discounts.service.ts
@@ -10,6 +10,19 @@ export class DiscountsService {
 
   constructor(private authService: AuthService) { }
 
+  getDiscountsList() {
+    return Axios.get( environment.backend_url + '/discount/discount_list', {
+      headers: {
+        Authorization: this.authService.token
+      }
+    })
+    .catch(error => {
+      if (error.response.data.detail == "Session expired, Reopen the application!") {
+        this.authService.signout();
+      }
+    });
+  }
+
   createDiscount(data) {
     return Axios.post( environment.backend_url + '/discount/discount', data, {
       headers: {
