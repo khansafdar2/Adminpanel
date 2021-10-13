6eb65cdf1967a5a67fa78dff0c12a62409d09d7a
moutasim <moutasimahmad24@gmail.com>
2021-10-13 12:27:59 +0500

    vendor lisitng

--- src/app/views/vendors/vendors.component.ts
+++ src/app/views/vendors/vendors.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import URLS from 'src/app/shared/urls';
+import { VendorsService } from './vendors.service';
+import { Column } from 'src/app/shared/datatable/datatable.component';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-vendors',
@@ -8,12 +11,126 @@ import URLS from 'src/app/shared/urls';
 })
 export class VendorsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private vendorsService: VendorsService
+  ) { }
 
   loading: boolean = false;
   URLS = URLS;
+  searchString: string = "";
+  pageSize: number = 20;
+  totalCount: number = 0;
+  page: number = 1;
+  vendors = []; 
+  filterString = '';
+
+  displayedColumns: Column[] = [
+    {
+      title: "Name",
+      selector: "name",
+      clickable: true
+    },
+    {
+      title: "Email",
+      selector: "email"
+    },
+    {
+      title: "Date",
+      selector: "created_at",
+      pipe: 'date',
+      dateFormat: 'h:mm a MMM d'
+    },
+    {
+      title: "Is Active",
+      selector: "is_active",
+      label: true,
+      labelStyles: {
+        "false": "default",
+        "true": "success"
+      }
+    }
+  ];
+
+  filtersArray = [
+    {
+      title: "Status",
+      key: "status",
+      values: [
+        {
+          label: "Active",
+          value: "active"
+        },
+        {
+          label: "Inactive",
+          value: "inactive"
+        }
+      ]
+    }
+    // ,
+    // {
+    //   title: "Approval status",
+    //   key: "approval_status",
+    //   values: [
+    //     {
+    //       label: "Approved",
+    //       value: "approved"
+    //     },
+    //     {
+    //       label: "Disapproved",
+    //       value: "disapproved"
+    //     }
+    //   ]
+    // }
+  ]
+
+  searchColumns = [
+    {
+      label: "Name",
+      value: "name"
+    },
+    {
+      label: "Email",
+      value: "email"
+    }
+  ];
+
+  onFilter(filters) {
+    let tempFilterString: string = "";
+    for (let i = 0; i < filters.length; i++) {
+      if(filters[i].value) {
+        tempFilterString += '&' + filters[i].key + '=' + filters[i].value;
+      }
+    }
+    this.filterString = tempFilterString;
+    this.getVendors();
+  }
+
+  onSearch(data) {
+    this.searchString = data.query.replaceAll("#", "") + "&column=" + data.column;
+    this.page = 1;
+    this.getVendors();
+  }
+
+  onPage(event: PageEvent) {
+    this.page = event.pageIndex + 1;
+    this.pageSize = event.pageSize;
+    this.getVendors();
+  }
+
+  getVendors() {
+    this.loading = true;
+    this.vendorsService.getVendorsList(this.page, this.pageSize, this.searchString, this.filterString).then(resp => {
+      this.loading = false;
+      if(resp) {
+        this.totalCount = resp.data.count;
+        this.vendors = resp.data.results;
+        console.log(resp)
+      }
+    });
+  }
 
   ngOnInit(): void {
+    this.getVendors();
   }
 
 }
