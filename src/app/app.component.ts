42f05ff563907252687512954f9ab2cf6d6d1330
Atta ur Rehman <atta-ur-rehman@alchemative.com>
2021-10-15 15:09:14 +0500

    version 3.6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,6 @@ export class AppComponent implements OnInit {
   title = 'kees-dashboard';
 
   ngOnInit() {
-    console.log("Kees Dashboard: v3.5");
+    console.log("Kees Dashboard: v3.6");
   }
 }
