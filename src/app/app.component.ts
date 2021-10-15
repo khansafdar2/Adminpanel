90861570665293a4a5736e11b0f211909ae378a9
Atta ur Rehman <atta-ur-rehman@alchemative.com>
2021-10-15 15:37:05 +0500

    Master pipline testing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,6 @@ export class AppComponent implements OnInit {
   title = 'kees-dashboard';
 
   ngOnInit() {
-    console.log("Dev ops ka issue.");
+    console.log("Dev Ops ka Bara issue.");
   }
 }
