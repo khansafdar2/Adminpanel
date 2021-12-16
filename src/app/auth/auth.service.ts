d8c6a9c6973fb0406932aef17efe067e78bc93d0
DESKTOP-DGFOOOO\HP <stud.waqa@gmail.com>
2021-12-16 12:49:37 +0500

    Code fixes

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,7 +6,7 @@ import URLS from '../shared/urls';
 interface UserPermission {
   id: number;
   dashboard: boolean;
-  customization: boolean
+  customization: boolean;
   theme: boolean;
   products: boolean;
   orders: boolean;
