8b64de3bb7a60f828c5c6e1217ab96a5f3b31a0f
Atta ur Rehman <atta-ur-rehman@alchemative.com>
2022-01-27 18:15:03 +0500

    version 4.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     document.title = environment.client_name + " dashboard";
-    console.log("Version 4.4 Shipping module.");
+    console.log("Version 4.5 Filters module.");
   }
 }
