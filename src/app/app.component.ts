4203f1072470a313991cf2f3bb507b26b92fa47f
Atta ur Rehman <atta-ur-rehman@alchemative.com>
2022-02-01 15:26:19 +0500

    version 4.7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     document.title = environment.client_name + " dashboard";
-    console.log("Version 4.6 Edit order.");
+    console.log("Version 4.7 Loyalty.");
   }
 }
