c156e280374a3df822992ca2c733044d41679ded
Atta ur Rehman <atta-ur-rehman@alchemative.com>
2022-02-02 18:08:30 +0500

    version 4.9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     document.title = environment.client_name + " dashboard";
-    console.log("Version 4.8 Filters position.");
+    console.log("Version 4.9 Loyalty fixes.");
   }
 }
