7b3ed369c48d999dbc7a2f3485bb63394c53c2e8
Atta ur Rehman <atta-ur-rehman@alchemative.com>
2022-02-16 05:16:50 +0500

    version 4.93

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     document.title = environment.client_name + " dashboard";
-    console.log("Version 4.92 Shipping regions");
+    console.log("Version 4.93 Discount testing.");
   }
 }
