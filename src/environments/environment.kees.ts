cc19fb2f99b8143e1d0d3859d2744990ef2ba10b
Atta ur Rehman <atta-ur-rehman@alchemative.com>
2022-04-25 14:02:35 +0500

    Fix backend url

--- src/environments/environment.kees.ts
+++ src/environments/environment.kees.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   client_name: 'KEES',
-  // backend_url: "https://backend.kees.qa",
-  backend_url: "https://staging-backend.kees.qa",
+  backend_url: "https://backend.kees.qa",
+  // backend_url: "https://staging-backend.kees.qa",
   // backend_url: "http://b5f3-110-93-244-53.ngrok.io",
   client_img_folder: 'kees',
   currency: "QAR"
